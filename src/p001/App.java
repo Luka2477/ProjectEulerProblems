d874329ab978d63a19f240e152e5c75e80b872c2
Luka2477 <luke.da.duke@hotmail.com>
2022-01-20 11:42:00 +0100

    Repository INIT

---
+++ src/p001/App.java
@@ -0,0 +1,13 @@
+package p001;
+
+public class App {
+
+    public static int sumOfMultiplesOf3And5 (int maxBound) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumOfMultiplesOf3And5(1000));
+    }
+
+}
